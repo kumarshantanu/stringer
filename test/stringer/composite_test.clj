cd961a4f6cb87ab668014d8e3d972bea75fe3a17
Shantanu Kumar <kumar.shantanu@gmail.com>
2015-04-27 01:05:11 +0530

    refactor tests into simple and composite using common harness

---
+++ test/stringer/composite_test.clj
@@ -0,0 +1,63 @@
+(ns stringer.composite-test
+  (:require
+    [clojure.test :refer :all]
+    [clojure.string        :as t]
+    [stringer.core         :as s]
+    [stringer.test-data    :as d]
+    [stringer.test-harness :as h]))
+
+
+(use-fixtures :once (h/make-bench-test-wrapper "bench-composite.png"))
+
+
+(deftest ^{:perf true :strcat true} test-concat-large-text
+  (testing "large text"
+    (h/compare-perf
+      (str d/lorem-ipsum d/lorem-ipsum d/lorem-ipsum)
+      (stringer.core/strcat d/lorem-ipsum d/lorem-ipsum d/lorem-ipsum))))
+
+
+(deftest test-largetext-join
+  (testing "large-text"
+    (is (=
+          (clojure.string/join ", " [d/lorem-ipsum d/lorem-ipsum d/lorem-ipsum])
+          (stringer.core/strjoin ", " d/lorem-ipsum d/lorem-ipsum d/lorem-ipsum)))))
+
+
+(deftest ^{:perf true :strjoin true} test-largetext-join-perf
+  (testing "large-text"
+    (h/compare-perf
+      (clojure.string/join ", " [d/lorem-ipsum d/lorem-ipsum d/lorem-ipsum])
+      (stringer.core/strjoin ", " d/lorem-ipsum d/lorem-ipsum d/lorem-ipsum))))
+
+
+(defn composite-str
+  [m]
+  (t/join ", " [(str "Foo " (:foo m))
+                (str "Bands: " (t/join ", " (:bar m)))
+                (str "Diff: " (- (get-in m [:baz :norf])
+                                (get-in m [:baz :quux])))]))
+
+
+(defn composite-strcat
+  [m]
+  (s/with-obj-str w
+    (s/append! w
+      "Foo " (:foo m)
+      ", "
+      "Bands: " (t/join ", " (:bar m))
+      ", "
+      "Diff: " (- (get-in m [:baz :norf])
+                 (get-in m [:baz :quux])))))
+
+
+(def composite-data {:foo "fighters"
+                     :bar ["U2" "MLTR"]
+                     :baz {:quux 10
+                           :norf 20}})
+
+
+(deftest ^{:perf true :strjoin true} test-composite-concat
+  (h/compare-perf
+    (composite-str composite-data)
+    (composite-strcat composite-data)))
--- test/stringer/core_test.clj
+++ test/stringer/core_test.clj
@@ -1,101 +1,11 @@
 (ns stringer.core-test
-  (:require [clojure.test :refer :all]
-            [clojure.pprint     :as pp]
-            [clojure.string     :as t]
-            [stringer.core      :as s]
-            [stringer.test-data :as d]
-            [cljfreechart.core  :as b]
-            [criterium.core     :as c]
-            [clansi.core        :as a]))
+  (:require
+    [clojure.test :refer :all]
+    [stringer.core         :as s]
+    [stringer.test-harness :as h]))
 
 
-(def bar-chart-data (atom []))
-
-
-(defn wrap-setup
-  [f]
-  (f)
-  (-> (let [raw-data @bar-chart-data
-            make-row (fn [k] (reduce (fn [x {:keys [fast-name] :as y}]
-                                       (assoc x fast-name (get y k)))
-                               {} raw-data))]
-        [(assoc (make-row :slow-mean) :name "Core")
-         (assoc (make-row :fast-mean) :name "Stringer")])
-    (b/make-category-dataset {:group-key :name})
-    (b/make-bar-chart-3d "Stringer benchmark statistics (lower is better)" {:category-title "Test cases"
-                                                                            :value-title "Latency (lower is better)"})
-    (b/save-chart-as-file "bench.png" {:width 1280 :height 800})))
-
-
-(use-fixtures :once wrap-setup)
-
-
-(defn save-bar-chart-data!
-  "Save given map as bar-chart data."
-  [slow-name slow-mean fast-name fast-mean]
-  (swap! bar-chart-data conj {:slow-mean slow-mean
-                              :fast-mean fast-mean
-                              :slow-name slow-name
-                              :fast-name fast-name}))
-
-
-(defmacro measure
-  [expr]
-  `(do
-     (println "\n::::: Benchmarking" ~(pr-str expr))
-     (let [result# (c/benchmark ~expr {})]
-       [result# (with-out-str (c/report-result result#))])))
-
-
-(defn nix?
-  []
-  (let [os (t/lower-case (str (System/getProperty "os.name")))]
-    (some #(>= (.indexOf os ^String %) 0) ["mac" "linux" "unix"])))
-
-
-(defn colorize
-  [text & args]
-  (if (nix?)
-    (apply a/style text args)
-    text))
-
-
-(defn comparison-summary
-  [slow-bench fast-bench]
-  (let [slow-mean (first (:mean slow-bench))
-        fast-mean (first (:mean fast-bench))
-        diff (Math/abs ^double (- slow-mean fast-mean))
-        calc-percent (fn [] (double (/ (* 100 diff)
-                                      (if (> slow-mean fast-mean)
-                                        fast-mean
-                                        slow-mean))))]
-    (cond
-      (= slow-mean fast-mean) (colorize "Both are equal" :yellow)
-      (> slow-mean fast-mean) (colorize (format "Stringer is faster by %.2f%% in this test." (calc-percent) \%)
-                                :black :bg-green)
-      :otherwise              (colorize (format "Stringer is slower by %.2f%% in this test." (calc-percent) \%)
-                                :black :bg-red))))
-
-
-(defmacro compare-perf
-  [slow-expr fast-expr]
-  `(do
-     (is (= ~slow-expr ~fast-expr))
-     (let [[slow-bench# slow-report#] (measure ~slow-expr)
-           [fast-bench# fast-report#] (measure ~fast-expr)]
-       (let [slow-label# ~(pr-str slow-expr)
-             fast-label# ~(pr-str fast-expr)]
-         (->> [slow-report# fast-report#]
-           (map t/split-lines)
-           (apply map (fn [s# f# ] {slow-label# s#
-                                    fast-label# f#}))
-           (pp/print-table [slow-label# fast-label#])))
-       (println (comparison-summary slow-bench# fast-bench#))
-       (save-bar-chart-data!
-         ~(pr-str slow-expr) (first (:mean slow-bench#))
-         ~(pr-str fast-expr) (first (:mean fast-bench#)))
-       (is (>= (first (:mean slow-bench#))
-             (first (:mean fast-bench#)))))))
+(use-fixtures :once (h/make-bench-test-wrapper "bench-small.png"))
 
 
 (deftest test-with-obj-str
@@ -137,27 +47,23 @@
 
 (deftest ^{:perf true :strcat true} test-strcat-perf
   (testing "one-arg"
-    (compare-perf (str 34) (stringer.core/strcat 34))
+    (h/compare-perf (str 34) (stringer.core/strcat 34))
     (let [thirty-four 34]
-      (compare-perf (str thirty-four) (stringer.core/strcat thirty-four))))
+      (h/compare-perf (str thirty-four) (stringer.core/strcat thirty-four))))
   (testing "small tokens"
-    (compare-perf (str "foo" "bar" "baz") (stringer.core/strcat "foo" "bar" "baz"))
+    (h/compare-perf (str "foo" "bar" "baz") (stringer.core/strcat "foo" "bar" "baz"))
     (let [foo "foo" bar "bar" baz "baz"]
-      (compare-perf (str foo bar baz) (stringer.core/strcat foo bar baz))))
+      (h/compare-perf (str foo bar baz) (stringer.core/strcat foo bar baz))))
   (testing "various tokens"
-    (compare-perf (str 34 :er nil \- "foo") (stringer.core/strcat 34 :er nil \- "foo"))
+    (h/compare-perf (str 34 :er nil \- "foo") (stringer.core/strcat 34 :er nil \- "foo"))
     (let [thirty-four 34
           er-keyword :er
           null nil
           dash-char \-
           foo-str "foo"]
-      (compare-perf
+      (h/compare-perf
         (str thirty-four er-keyword null dash-char foo-str)
-        (stringer.core/strcat thirty-four er-keyword ^Object null dash-char foo-str))))
-  (testing "large text"
-    (compare-perf
-      (str d/lorem-ipsum d/lorem-ipsum d/lorem-ipsum)
-      (stringer.core/strcat d/lorem-ipsum d/lorem-ipsum d/lorem-ipsum))))
+        (stringer.core/strcat thirty-four er-keyword ^Object null dash-char foo-str)))))
 
 
 (deftest test-strjoin
@@ -185,26 +91,22 @@
           foo-str "foo"]
       (is (=
             (clojure.string/join ", " [one er-keyword null newline-char false-bool foo-str])
-            (stringer.core/strjoin ", " one er-keyword ^Object null newline-char false-bool foo-str)))))
-  (testing "large-text"
-    (is (=
-          (clojure.string/join ", " [d/lorem-ipsum d/lorem-ipsum d/lorem-ipsum])
-          (stringer.core/strjoin ", " d/lorem-ipsum d/lorem-ipsum d/lorem-ipsum)))))
+            (stringer.core/strjoin ", " one er-keyword ^Object null newline-char false-bool foo-str))))))
 
 
 (deftest ^{:perf true :strjoin true} test-strjoin-perf
   (testing "one-arg"
-    (compare-perf (clojure.string/join ", " [1]) (stringer.core/strjoin ", " 1))
+    (h/compare-perf (clojure.string/join ", " [1]) (stringer.core/strjoin ", " 1))
     (let [one 1]
-      (compare-perf (clojure.string/join ", " [one]) (stringer.core/strjoin ", " one))))
+      (h/compare-perf (clojure.string/join ", " [one]) (stringer.core/strjoin ", " one))))
   (testing "multi-args"
-    (compare-perf (clojure.string/join ", " [1 2 3]) (stringer.core/strjoin ", " 1 2 3))
+    (h/compare-perf (clojure.string/join ", " [1 2 3]) (stringer.core/strjoin ", " 1 2 3))
     (let [one 1
           two 2
           three 3]
-      (compare-perf (clojure.string/join ", " [one two three]) (stringer.core/strjoin ", " one two three))))
+      (h/compare-perf (clojure.string/join ", " [one two three]) (stringer.core/strjoin ", " one two three))))
   (testing "various-args"
-    (compare-perf
+    (h/compare-perf
       (clojure.string/join ", " [1 :er nil \newline false "foo"])
       (stringer.core/strjoin ", " 1 :er nil \newline false "foo"))
     (let [one 1
@@ -213,10 +115,6 @@
           newline-char \newline
           false-bool false
           foo-str "foo"]
-      (compare-perf
+      (h/compare-perf
         (clojure.string/join ", " [one er-keyword null newline-char false-bool foo-str])
-        (stringer.core/strjoin ", " one er-keyword ^Object null newline-char false-bool foo-str))))
-  (testing "large-text"
-    (compare-perf
-      (clojure.string/join ", " [d/lorem-ipsum d/lorem-ipsum d/lorem-ipsum])
-      (stringer.core/strjoin ", " d/lorem-ipsum d/lorem-ipsum d/lorem-ipsum))))
+        (stringer.core/strjoin ", " one er-keyword ^Object null newline-char false-bool foo-str)))))
