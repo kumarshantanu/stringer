d2fac9956743d8717b960f2cf06bdfaf41a57764
Shantanu Kumar <kumar.shantanu@gmail.com>
2015-08-14 13:34:47 +0530

    update citius and Clojure versions

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/kumarshantanu/stringer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[citius "0.1.0"]
+  :profiles {:dev {:dependencies [[citius "0.2.2"]
                                   [org.clojure/tools.nrepl "0.2.10"]]}
              :c16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :c17 {:dependencies [[org.clojure/clojure "1.7.0-RC1"]]
+             :c17 {:dependencies [[org.clojure/clojure "1.7.0"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}}
   :jvm-opts ^:replace ["-server" "-Xms2048m" "-Xmx2048m"]
   :global-vars {*warn-on-reflection* true
